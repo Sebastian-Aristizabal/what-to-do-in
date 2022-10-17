cf808c8bffdf255e3dfacbd6005c9e08179dc702
Sebastian Aristizabal <sebastinn@hotmail.com>
2022-10-17 17:12:21 -0500

    las ultimas modificaciones que hice

--- app/controllers/selected_plans_controller.rb
+++ app/controllers/selected_plans_controller.rb
@@ -9,13 +9,15 @@ class SelectedPlansController < ApplicationController
   end
 
   def create
-    raise
-    @selected = SelectedPlan.new(selected_params)
-    @selected.user_id = current_user.id
     @plan = Plan.find(params[:plan_id])
+    @selected = SelectedPlan.new
+    # @selected = SelectedPlan.new(user_id: current_user.id, plan_id: 7, status: "hola")
     @selected.plan_id = @plan.id
+    @selected.user_id = current_user.id
+    # @plan = Plan.find(params[:plan_id])
+    # @selected.plan_id = @plan.id
     if @selected.save
-      redirect_to selected_plans
+      redirect_to selected_plans_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -24,6 +26,6 @@ class SelectedPlansController < ApplicationController
   private
 
   def selected_params
-    params.require(:selected_plans).permit(:user_id, :plan_id, :status)
+    params.require(:selected_plan).permit(:plan_id, :user_id)
   end
 end
--- app/models/selected_plan.rb
+++ app/models/selected_plan.rb
@@ -1,4 +1,9 @@
 class SelectedPlan < ApplicationRecord
+  enum status: {
+    quiero_asistir: 0,
+    asistí: 1,
+    no_asistí: 2
+  }
   belongs_to :user
   belongs_to :plan
 end
