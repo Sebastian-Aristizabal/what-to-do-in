b45143eb28f1216842cf28c088e3ec09728e43fc
Daniel Rendon <danirendon512@gmail.com>
2022-10-17 20:30:42 -0500

    conrollador de reviws, y vistas de los formularios

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @plan = Plan.find(params[:plans_id])
+    @review = Review.new(review_params)
+    @review.plan = @plan
+    if @review.save
+      redirect_to plan_path(@review.plan)
+    else
+      render "plans#show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to plan_path(@review.plan), status: :see_other
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to plan_path(@review.plan)
+    else
+      render "plans#show", status: :unprocessable_entity
+    end
+  end
+
+  
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment, :plan_id, :user_id)
+  end
 end
