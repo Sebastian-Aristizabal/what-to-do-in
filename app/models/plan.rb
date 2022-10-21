ff66ff7b773bbb34ca37cb16bf5c08a3de895949
andrea guzman <andreaguzmanr27@outlook.com>
2022-10-20 23:11:59 -0500

    search and navbar completed

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,4 +4,14 @@ class Plan < ApplicationRecord
   has_many :selected_plans, dependent: :destroy
   belongs_to :category
   belongs_to :user
+
+  include PgSearch::Model
+  pg_search_scope :search,
+  against: [:title, :details, :address],
+  associated_against: {
+    category: [:name]
+  },
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
