c2533acd26c4cc7a6bf94711230506e288365b74
Daniel Rendon <danirendon512@gmail.com>
2022-10-21 15:29:05 -0500

    correcciones-seed

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -20,17 +20,17 @@ class Plan < ApplicationRecord
   using: {
     tsearch: { prefix: true } # <-- now `superman batm` will return something!
   }
-  def date_invalid
-    if self.start_date.present? && self.start_date < Date.today
-      self.errors.add(:start_date, "la fecha inicial no puede ser menor a la fecha actual")
+   def date_invalid
+      if self.start_date.present? && self.start_date < Date.today
+        self.errors.add(:start_date, "la fecha inicial no puede ser menor a la fecha actual")
+      end
+      if self.end_date < self.start_date
+        self.errors.add(:start_date, "la fecha inicial no puede ser mayor a la fecha final")
+      end
     end
-    if self.end_date < self.start_date
-      self.errors.add(:start_date, "la fecha inicial no puede ser mayor a la fecha final")
-    end
-  end
 
   def hour_invalid
-    if self.start_hour.present? && self.start_hour < Time.now
+    if self.start_hour.present? && self.start_hour > Time.now
       self.errors.add(:start_hour, "la hora inicial no puede ser menor a la hora actual")
     end
     if self.end_hour < self.start_hour && self.end_date == start_date
