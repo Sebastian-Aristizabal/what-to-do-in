87a9516fffcdad5b30634a9d178cec171f3756af
Sebastian Aristizabal <sebastinn@hotmail.com>
2022-10-25 22:04:43 -0500

    rating-start, formulario de plans con las validaciones organizadas

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -30,14 +30,12 @@ class Plan < ApplicationRecord
     end
 
   def hour_invalid
-    if self.start_hour.present? && ((self.start_hour < Time.now) && (self.start_date <= Date.today))
-      self.errors.add(:start_hour, "la hora inicial no puede ser menor a la hora actual \n")
-    end
-    if self.end_hour.present? && ((self.end_hour < Time.now) && (self.end_date <= Date.today))
-      self.errors.add(:end_hour, "la hora final no puede ser menor a la hora actual \n")
-    end
-    if self.end_hour < self.start_hour && self.end_date == self.start_date
-      self.errors.add(:end_hour, "la hora final no puede ser menor a la hora inicial para el día de hoy \n")
+    unless start_date != end_date
+      if self.start_hour.present?
+        if (self.start_hour < Time.now) && (self.start_date <= Date.today)
+          self.errors.add(:start_hour, "la hora inicial no puede ser menor a la hora actual \n")
+        end
+      end
     end
   end
 end
