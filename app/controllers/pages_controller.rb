c4a351474a2416296f160b5f4bfe8756ecd3ec4e
Sebastian Aristizabal <sebastinn@hotmail.com>
2022-10-26 22:40:20 -0500

    se creo la logica para que muestre en el home los 4 planes mas deseados de mayor a menor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,12 @@ class PagesController < ApplicationController
   def home
     @category = Category.all
     @plans = Plan.all
+    top_selected_plans
   end
+  private
+  
+  def top_selected_plans
+    @selected_top = SelectedPlan.where(status: 0).group_by{|plan| plan.plan_id}.sort_by{|key| key.count}.reverse.first(4)
+  end
+
 end
