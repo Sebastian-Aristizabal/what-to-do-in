ab637c39b4d08843554d919362cc344bf5618081
Sebastian Aristizabal <sebastinn@hotmail.com>
2022-10-28 16:36:00 -0500

    el metodo top_selected_plans en el controlador de pages, FUNCIONANDO bien y separado para el primero y para que los otros 3 funcionen de forma independiente; también se hizo un breadcrumb aun falta terminarlo

--- app/controllers/selected_plans_controller.rb
+++ app/controllers/selected_plans_controller.rb
@@ -34,18 +34,6 @@ class SelectedPlansController < ApplicationController
     redirect_to selected_plans_path, status: :see_other
   end
 
-  def top_selected_plans
-    @selected_top = SelectedPlan.where(status: 0).group_by{|plan| plan.plan_id}.sort_by{|key| key.count}.reverse.first(4)
-    # @selected_top = SelectedPlan.group(:plan_id).count.sort_by{|plan| plan[1]}.reverse.first(4)
-    # SelectedPlan.group(:plan_id).count.order{|key, value| value}.reverse.limit(4)
-    # SelectedPlan.where(status: 0).group(:plan_id).count.order.reverse.limit(4)
-    # SelectedPlan.group(:plan_id).count.sort_by{|plan| plan[1]}.reverse.first(4)
-    # SelectedPlan.group(:plan_id).count.sort_by{|plan| plan[1]}.reverse.first(4)
-    # SelectedPlan.where(status: 0).group(:plan_id).count.sort_by(&:count)
-    # SelectedPlan.where(status: 0).group(:plan_id).count.sort_by(&:count)
-    # SelectedPlan.where(status: 0).group(:plan_id).count.sort_by{|plan| plan[1]}.reverse.first(4)
-  end
-
   private
 
   def selected_params
