ca819b90a8f3dbaaa74ca96c13fbd1f171d2df01
Daniel Rendon <danirendon512@gmail.com>
2022-10-31 17:43:55 -0500

    se cargo el mapa, y se comento las lineas para el seed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,10 @@ import "./controllers"
 import "bootstrap"
 
 
-import Flatpickr from 'stimulus-flatpickr'
+// import Flatpickr from 'stimulus-flatpickr'
 
 // Import style for flatpickr
-require("flatpickr/dist/flatpickr.css")
+// require("flatpickr/dist/flatpickr.css")
 
 // Manually register Flatpickr as a stimulus controller
 application.register('flatpickr', Flatpickr)
